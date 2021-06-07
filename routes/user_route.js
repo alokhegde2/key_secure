48ce0441da02e366a1da3de70bf26e12961570b7
alokhegde2 <hegdealok@yahoo.com>
2021-06-07 21:59:34 +0530

    Added register a user route 💥

--- routes/user_route.js
+++ routes/user_route.js
@@ -1,12 +1,54 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+const User = require("../models/user_model");
+const {
+  registerValidation,
+  loginValidation,
+  masterPasswordValidation,
+} = require("../helpers/validation");
 
 //All user routes goes here
 
-router.get('/:id',(req,res) => {
-    res.send("Hello this is user test route")
-})
+//Register the new user
+router.post("/register", async (req, res) => {
+  //Validating the data before creating the user
+
+  const { error } = registerValidation(req.body);
+  if (error) {
+    return res.status(400).json({"message":error.details[0].message});
+  }
+
+  //Checking if email is already exist in the database
+  const emailExist = await User.findOne({ email: req.body.email });
+
+  if (emailExist) {
+    return res.status(400).json({ message: "Email already exist" });
+  }
+
+  //Hashing the password
+  //creating salt for hashing
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+  //reciving all data in body
+
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    hashedPassword: hashPassword,
+    authType: req.body.authType,
+  });
+
+  try {
+    savedUser = await user.save();
+    res.status(200).send({ userId: user.id });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 //Exporting the user module
 module.exports = router;
