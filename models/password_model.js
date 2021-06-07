cc45f6aaa2246cc2ae391015ec293d66133c7ab2
alokhegde2 <hegdealok@yahoo.com>
2021-06-07 18:50:55 +0530

    Added mongodb connection 🚀

--- models/password_model.js
+++ models/password_model.js
@@ -1,67 +1,65 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 //Schema
 
 const passwordSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true,
-        min:2,
-        max:255
-    },
-    username:{
-        title:String,
-        min:5,
-        max:255
-    },
-    password:{
-        type:String,
-        required:true,
-        min:8
-    },
-    emailId:{
-        type:String,
-        required:true,
-        min:5
-    },
-    category:{
-        type:String,
-        required:true,
-        min:3
-    },
-    isImportant:{
-        type:Boolean,
-        default:false
-    },
-    note:{
-        type:String,
-        default:""
-    },
-    userId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        require:true
-    },
-    date:{
-        type:Date,
-        default:Date.now()
-    }
+  title: {
+    type: String,
+    required: true,
+    min: 2,
+    max: 255,
+  },
+  username: {
+    title: String,
+    max: 255,
+    default: "",
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 8,
+  },
+  emailId: {
+    type: String,
+    required: true,
+    min: 5,
+  },
+  category: {
+    type: String,
+    required: true,
+    min: 3,
+  },
+  isImportant: {
+    type: Boolean,
+    default: false,
+  },
+  note: {
+    type: String,
+    default: "",
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    require: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
-
 //Creating virtual id
-passwordSchema.virtual('id').get(() =>{
-    return this._id.toHexString();
+passwordSchema.virtual("id").get(() => {
+  return this._id.toHexString();
 });
 
 //Creating virtual userId
-passwordSchema.virtual('userId').get(()=>{
-    return this.userId.toHexString();
+passwordSchema.virtual("userId").get(() => {
+  return this.userId.toHexString();
 });
 
-passwordSchema.set('toJSON',{
-    virtuals:true,
+passwordSchema.set("toJSON", {
+  virtuals: true,
 });
 
-module.exports = mongoose.model('Password',passwordSchema);
+module.exports = mongoose.model("Password", passwordSchema);
