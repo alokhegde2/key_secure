32a7c6b9a882f150b2f786dd7cca8c9d1c118955
alokhegde2 <hegdealok@yahoo.com>
2021-06-07 21:10:17 +0530

    Added validate.js file for validation 🔥

---
+++ helpers/validation.js
@@ -0,0 +1,39 @@
+const Joi = require("joi");
+
+//Register validation
+
+const registerValidation = (data) => {
+  const schema = Joi.object({
+    name: Joi.string().min(2),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  });
+
+  return schema.validate(data);
+};
+
+//Login validation error
+
+const loginValidation = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  });
+
+  return schema.validate(data);
+};
+
+//Master Password validation
+
+const masterPasswordValidation = (data) => {
+  const schema = Joi.object({
+    masterPassword: Joi.string().min(4).max(4),
+  });
+
+  return schema.validate(data);
+};
+
+//Exporting modules
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
+module.exports.masterPasswordValidation = masterPasswordValidation;
