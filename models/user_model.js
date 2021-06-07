4f39eac7a444d6425ac6f5793931a4894dcb2964
alokhegde2 <hegdealok@yahoo.com>
2021-06-07 20:28:41 +0530

    Completed user_model.js 💥

---
+++ models/user_model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+//User Schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 255,
+  },
+  email: {
+    type: String,
+    required: true,
+    min: 5,
+    max: 50,
+  },
+  hashedPassword: {
+    type: String,
+    required: true,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  masterPassword: {
+    type: String,
+    default: "",
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+//Creating virtual id
+userSchema.virtual("id").get(() => {
+  return this._id.toHexString();
+});
+
+userSchema.set("toJSON", {
+  virtuals: true,
+});
+
+//Exporting modules
+module.exports = mongoose.model("User", userSchema);
