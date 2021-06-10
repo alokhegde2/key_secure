a5f5b47fb28d017d1de3ad4ac332033dad7bda99
alokhegde2 <hegdealok@yahoo.com>
2021-06-10 20:25:59 +0530

    Removed mailing functionality from register_user🧡

--- routes/register_user.js
+++ routes/register_user.js
@@ -9,77 +9,12 @@ const path = require("path");
 require("dotenv/config");
 
 const User = require("../models/user_model");
+const Mail = require("../helpers/mail");
 const {
   registerValidation,
   newMasterPasswordValidation,
 } = require("../helpers/validation");
 const { func } = require("joi");
-
-const api = process.env.API_URL;
-
-//Sending confirmation mail function
-async function SendMail(token, email, name) {
-  //url for confirming mail
-  const url = `${req.protocol}://${req.get(
-    "host"
-  )}/${api}/register/verify-user/${token}`;
-
-  //body of the mail
-  const body = `
-  <body style="background-color:#EEEEEE;border-radius:20px">
-    <div style="margin:20px;display:flex;flex-direction:row;flex-wrap: wrap;justify-content: space-around;">
-      <h3 style="color:#3A4043">Key Secure</h3>
-      <img src="https://github.com/alokhegde2/key-secure/raw/main/images/logo.svg" alt="" width="50px">
-    </div>
-    <div style="margin:10px;border:5px solid #859AFF">
-      <center>
-        <h4 style="color:#3A4043">Hi ${name},</h4>
-        <p style="color:#363636">You have successfully created a Key Secure account,<br/>Please click on the link below to verify your email<br/> address and complete your registration</p>
-    <br/>
-    <a href="${url}"><button style="background-color:#768bf5;border:none;padding:10px;margin-bottom:10px;color:white;border-radius:5px;cursor:pointer">Verify your email</button></a><br/>
-    <small style="color:#7d7d7d">or copy and paste this link into your browser:</small><br/>
-    <a href="${url}"style="font-size:12px;color:#76a0f5;">${url}</a><br/><br/>
-    <small style="color:#7d7d7d;">Didn't create a Key Seure account? It's likely someone just typed in your email<br/>address by accident.Feel free to ignore this email</small><br/><br/>
-      </center>
-    </div>
-    <div style="margin:20px;display:flex;flex-direction:row;flex-wrap: wrap;justify-content: space-around;">
-      <small style="color:#3A4043">&copy 2021 Key Secure</small>
-      <small style="color:#7d7d7d;">Follow us on <a href="https://github.com/alokhegde2/" style="color:#7d7d7d;">Github</a></small>
-    </div>
-    </body>
-  `;
-
-  //Admin mail and password
-  const admin_mail = process.env.MAIL_ID;
-  const admin_pass = process.env.PASSWORD;
-  console.log(admin_mail, admin_pass);
-
-  let transporter = nodemailer.createTransport({
-    // host: "mail.google.com",
-    service: "Gmail",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: admin_mail, // generated gmail user
-      pass: admin_pass, // generated gmail password
-    },
-  });
-
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: `"Key Secure"  <${admin_mail}>`, // sender address
-    to: `${email}`, // list of receivers
-    subject: "Please confirm your account", // Subject line
-    html: body, // html body
-  });
-
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
 
 //All user routes goes here
 
@@ -120,7 +55,7 @@ router.post("/", async (req, res) => {
 
   try {
     savedUser = await user.save();
-    SendMail(token, req.body.email, req.body.name);
+    Mail(token, req.body.email, req.body.name, "confirm");
     res.status(200).send({ message: "User registered successfully" });
   } catch (error) {
     res.status(400).send(error);
