da5d6badfabf1a692aad6222141e7e16478d48d7
alokhegde2 <hegdealok@yahoo.com>
2021-06-21 23:05:38 +0530

    Added password route page

--- routes/password_route.js
+++ routes/password_route.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require("mongoose");
+const path = require("path");
 
+//importing dotenv
+require("dotenv/config");
+
+//importing models
+const Password = require("../models/password_model");
+const User = require("../models/user_model");
+const Category = require("../models/category_model");
+//importing verify middleware
+const verify = require("../helpers/verify_token");
+//importing validation
+const {newPassword} = require("../helpers/validation");
 
 //All password routes goes here
 
-router.get('/all-pass',(req,res) =>{
-    res.send("Gives all the password")
+//Creating new password
+router.post("/new-password",verify,async (req,res) => {
+
 });
 
 
