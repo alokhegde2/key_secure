97bf32eef755d11999ff75679453f75b69f9b716
alokhegde2 <hegdealok@yahoo.com>
2021-06-21 18:49:20 +0530

    Added validation for creating new category

--- helpers/validation.js
+++ helpers/validation.js
@@ -60,6 +60,20 @@ const changeMasterPassword = (data) =>{
   return schema.validate(data);
 }
 
+
+//CATEGORY VALIDATION
+
+//validating new category
+const newCategory = (data) =>{
+  const schema = Joi.object({
+    name:Joi.string().max(20).min(6).required(),
+    icon:Joi.string().default(""),
+    color:Joi.string().max(10),
+    user_id:Joi.string().required(),
+  });
+  return schema.validate(data);
+}
+
 //Exporting modules
 module.exports.registerValidation = registerValidation;
 module.exports.loginValidation = loginValidation;
@@ -67,3 +81,4 @@ module.exports.newMasterPasswordValidation = newMasterPasswordValidation;
 module.exports.resetMasterPasswordValidation = resetMasterPasswordValidation;
 module.exports.changePassword = changePassword;
 module.exports.changeMasterPassword = changeMasterPassword;
+module.exports.newCategory = newCategory;
