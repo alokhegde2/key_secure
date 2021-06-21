f651e7ecd86d3c2a615ae1949358764a2a82d5c7
alokhegde2 <hegdealok@yahoo.com>
2021-06-21 21:03:40 +0530

    Added update category route

--- routes/category_route.js
+++ routes/category_route.js
@@ -12,7 +12,7 @@ const User = require("../models/user_model");
 //importing verify middleware
 const verify = require("../helpers/verify_token");
 //importing validation
-const { newCategory } = require("../helpers/validation");
+const { newCategory, updateCategory } = require("../helpers/validation");
 
 //All routes goes here
 
@@ -40,6 +40,13 @@ router.post("/new-category", verify, async (req, res) => {
     return res.status(400).json({ message: "User not found" });
   }
 
+  //check for category name already present or not
+  const category_name = category.name.toLowerCase();
+  const entered_name = req.body.name.toLowerCase();
+  if (category_name == entered_name) {
+    return res.status(400).json({ message: "Category name already exist" });
+  }
+
   //Creating new categorys
   const new_category = new Category({
     name: req.body.name,
@@ -64,7 +71,7 @@ router.get("/get-category/:user_id", verify, async (req, res) => {
   }
 
   //Checking for user is present or not
-  const user = await User.findOne({_id:req.params.user_id});
+  const user = await User.findOne({ _id: req.params.user_id });
 
   //if user not there
   if (!user) {
@@ -85,6 +92,53 @@ router.get("/get-category/:user_id", verify, async (req, res) => {
 });
 
 //Updating the category
+router.put("/update-category/:id", verify, async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    return res.status(400).json({ message: "Invalid category id" });
+  }
 
+  //if id is valid
+  //check for the entered data are valid
+  const { error } = updateCategory(req.body);
+
+  //if error found during validtion
+  if (error) {
+    return res.status(400).json({ message: `${error.details[0].message}` });
+  }
+
+  //if there is no error
+  //check the category present or not
+  const category = await Category.findById(req.params.id);
+
+  //if category is not present
+  if (!category) {
+    return res.status(400).json({ message: "Category not found" });
+  }
+
+  //if category present
+  //check for category name already present or not
+  const category_name = category.name.toLowerCase();
+  const entered_name = req.body.name.toLowerCase();
+  if (category_name == entered_name) {
+    return res.status(400).json({ message: "Category name already exist" });
+  }
+
+  //update the password
+  const updatedCategory = await Category.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      icon: req.body.icon,
+      color: req.body.color,
+    },
+    { new: true }
+  );
+
+  //if category is not updated
+  if (!updatedCategory) {
+    return res.status(400).json({ message: "Category is not updated" });
+  }
+  return res.status(200).json({ message: "Category updated successfully" });
+});
 
 module.exports = router;
