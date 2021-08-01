d5e74bc294698bfa1560be64b0b4b699d47c310f
Alok Hegde <hegdealok@yahoo.com>
2021-08-01 22:30:26 +0530

    Removed BodyParser

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -30,8 +29,8 @@ app.use(compression());
 //Middlewares
 //Middleware to serve static files
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
 app.use(morgan("tiny"));
 
 //All route middlewares goes here
